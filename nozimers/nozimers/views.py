938b744e36d56795598cc044f74ed84c5b96aae5
Yugal Sharma <yugalsharma13@gmail.com>
2017-10-14 17:03:15 +0530

    Update views.py

--- nozimers/nozimers/views.py
+++ nozimers/nozimers/views.py
@@ -3,13 +3,19 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
-import getids
+from . import getids
+from PIL import Image
+import base64
+from io import BytesIO
+import io
 
 
 def get_prediction(request):
-    #get the image here. Predict and return result
-    profile = predict_final_face(open("test/nitin.jpg", 'rb'))
-    img_url = "localhost:8000/nozimers/nozimers"
+    im = BytesIO(base64.b64decode(request.POST['image'].encode()))
+    print(type(im))
+    profile = getids.predict_final_face(im)
     profile['image'] = '/images/'+profile['image']
+    print(profile)
     return HttpResponse(profile)
     
+
